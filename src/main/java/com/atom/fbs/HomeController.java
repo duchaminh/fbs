9045ec8b58d58047dd51ad971525a1f495050146
Ha Minh Duc <hmdndbg96@gmail.com>
2018-08-08 07:58:29 +0700

    design user interface for user

--- src/main/java/com/atom/fbs/HomeController.java
+++ src/main/java/com/atom/fbs/HomeController.java
@@ -16,24 +16,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		// cai nay de test thoi a
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
+	public String home( Model model) {
 		return "home";
+	}
+	
+	@RequestMapping(value = "/shop", method = RequestMethod.GET)
+	public String shop( Model model) {
+		return "shop";
+	}
+	
+	@RequestMapping(value = "/cart")
+	public String cart( Model model) {
+		return "cart";
+	}
+	
+	@RequestMapping(value = "/checkout", method = RequestMethod.GET)
+	public String checkout( Model model) {
+		return "checkout";
 	}
 	
 }
